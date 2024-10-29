712c2d86fd5dc6a12d5d662f91ae8d3693da84f8
DiazVB2506 <23102008@utfv.edu.mx>
2024-10-29 16:19:27 -0600

    Update conexion1.php

--- conexion1.php
+++ conexion1.php
@@ -1,11 +1,9 @@
 <?php
 // Crear la conexión a la base de datos
-$cn = new mysqli("localhost", "root", "250602", "usuarios1", 3306); // Cambia "empleado1" a "usuarios1"
+$cn = new mysqli("localhost", "root", "tu_contraseña", "usuarios1", 3306); // Asegúrate de que la contraseña y el nombre de la base de datos sean correctos
 
 // Verificar si la conexión fue exitosa
 if ($cn->connect_error) {
     die("Conexión fallida: " . $cn->connect_error);
 }
-
-echo "Conexión exitosa.<br>";
 ?>
